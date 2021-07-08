5c632f3c36eb3a0c3511a439525752442b5f2992
ostrasburg <68702666+ostrasburg@users.noreply.github.com>
2021-07-08 13:36:45 -0400

    Add files via upload

--- updated/translation.py
+++ updated/translation.py
@@ -1,51 +1,58 @@
-import matplotlib
-matplotlib.use('agg')
-from matplotlib.backends.backend_pdf import PdfPages
-import matplotlib.pyplot as pyplot
 from Bio import SeqIO, Entrez, AlignIO
 from Bio.Seq import Seq
 from Bio.Align import MultipleSeqAlignment
 from Bio.SeqFeature import SeqFeature, FeatureLocation
-from dna_features_viewer import GraphicFeature, GraphicRecord, BiopythonTranslator
-import math
+import os
+
+SEQUENCES_DIR = "/Volumes/Data2/results/viruses/sequences"
 
 
-path = "/Volumes/Data2/specimens/Amacuzac-Mexico-13.LIN210A1627/results/sequences/Amacuzac-Mexico-13.LIN210A1627_hits_aligned.fasta"
+#path = "/Volumes/Data2/specimens/Amacuzac-Mexico-13.LIN210A1627/results/sequences/Amacuzac-Mexico-13.LIN210A1627_hits_unaligned.fasta"
 
-# with open(path, 'r') as file:
-#     count = 1
-#     for line in file:
-#         print(count)
-#         if line[0] != ">":
-#             print("New contig:\n")
-#             seq = Seq(line.replace('-', 'N'))
-#             for index in range(3):
-#                 end = math.floor(len(seq[index:])/3)*3
-#                 print(end)
-#                 print(seq[index:end].translate(to_stop=True))
-#         count += 1
+def getTranslation():
+    hitsFiles = []
+    paths = os.listdir(SEQUENCES_DIR)
 
-#
-# with open(path, 'r') as f:
-#     for line in f:
-#         if line[0] != '>':
-#             print("New contig:")
-#             seq = Seq(line.replace('-', 'N').strip())
-#             for index in range(3):
-#                 to_translate = seq[index:len(seq[index:]) - (len(seq[index:])%3)]
-#                 print(to_translate.translate(to_stop = True))
+    for fam in paths:
+        path = f"/Volumes/Data2/results/viruses/sequences/{fam}/sequences_{fam}_per_contig_unaligned.fasta"
+        outPath = f"/Volumes/Data2/results/viruses/sequences/{fam}/sequences_{fam}_per_contig_translated.fasta"
+        with open(path, 'r') as reader, open(outPath, 'w') as outfile:        #count = 1
+            for line in reader:
+                if line[0] == ">":
+                    startPos = []
+                    endPos = []
+                    title = line.split(".")[0] + "." + line.split(".")[1][0]
+                    for x in line.split("_")[-1].split(","):    #get start and end positions in DNA
+                        startPos.append(int(x.split("-")[0]))
+                        endPos.append(int(x.split("-")[1]))
+                else:
+                    seq = Seq(line.strip().replace('-', 'N'))
+                    rev = seq.reverse_complement()
 
+                    for frame in range(3):
+                        frameStart = []
+                        end = []
+                        revStart = []
+                        revEnd = []
+                        posStr = ""
+                        revPosStr = ""
+                        frameEnd = len(seq) - len(seq[frame:])%3
 
-sequence = "GTCAACAGGACGATCATGAAGCATTTTATCCGGTTCTTTAAGAATCGCAACTTACGACCGCGTATTTTATCAAAAGAAGAATTCATCGCCAACGTTCGAAATGATGCGGCTATAGGATCGTGGAGCAGGGATGTGCCATGGCGAGATGTGCAAGAAGCCATTCAGGACCAGTGCTTCTGGGATCCCATC"
-#seq2 = "ATGCT--TGTGACTGGAGTTCAGACGTGTGCTCTTCCGAT"
-seq = Seq(sequence.strip().replace('-', 'N'))
-startLoc = [seq.find('ATG')]
-for x in startLoc:
-    loc = seq.find('ATG',x+1)
-    #print("running")
-    if loc != -1:
-        startLoc.append(loc)
-print(startLoc)
-for loc in startLoc:
-    to_translate = seq[loc:len(seq[loc:]) - (len(seq[loc:])%3)]
-    print(seq[loc:].translate(to_stop = True,stop_symbol='*'))
+                        for x in range(len(startPos)):
+                            frameStart.append(startPos[x] + frame)
+                            end.append(endPos[x] - len(seq[frame:])%3)
+                            posStr = posStr + str(frameStart[x]) + "-" + str(end[x]) + ","
+
+                            revStart.append(endPos[x] - frame)
+                            revEnd.append(startPos[x] + len(rev[frame:])%3)
+                            revPosStr = revPosStr + str(revStart[x]) + "-" + str(revEnd[x]) + ","
+
+                        tempSeq = seq[frame:frameEnd]
+                        outfile.write(title + "_" + posStr[:-1] + "_(+)\n")
+                        outfile.write(str(tempSeq.translate()) + "\n")
+
+                        revSeq = rev[frame:frameEnd]
+                        outfile.write(title + "_" + revPosStr[:-1] + "_(-)\n")
+                        outfile.write(str(revSeq.translate()) + "\n")
+
+getTranslation()
